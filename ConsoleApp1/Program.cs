904ed6dee9cd524b3d8cb83d956031a6590e82ae
vikto <vikto@DESKTOP-CBSCN3M>
2021-11-12 16:47:00 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class Guys
+    {
+            static void Main(string[] args)
+            {
+                StreamWriter sw = new StreamWriter("text.txt");
+                sw.WriteLine("Иванов Сергей Николаевич 21 64");
+                sw.WriteLine("Петров Игорь Юрьевич 45 88");
+                sw.WriteLine("Семёнов Михаил Алексеевич 20 70");
+                sw.WriteLine("Пиманов А");
+                sw.Close();
+
+                List<string[]> people = new List<string[]>();
+                StreamReader sr = new StreamReader("text.txt");
+
+                while (!sr.EndOfStream)
+                {
+                    string temp = sr.ReadLine();
+                    people.Add(temp.Split(' '));
+                }
+                try
+                {
+                    var func = people.Where(q => int.Parse(q[3]) % 3 == 0);
+                    foreach (string[] q in func)
+                    {
+                        Console.WriteLine(string.Join(" ", q));
+
+                    }
+                }
+                catch 
+                { 
+                Console.WriteLine("Персона имеет неверные параметры"); 
+                }
+                Console.WriteLine();
+                Console.WriteLine("Конец программы");
+            }  
+    }
+}
+
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] razn = new double[3];
+            string numbers = "1234567890";
+            bool yesno = false;
+            Console.WriteLine("Введите число");
+            for (int i = 0; i < 3; i++)
+            {
+                string temp = Console.ReadLine();
+                if (numbers.Contains(temp))
+                {
+                    razn[i] = Convert.ToDouble(temp);
+                }
+                else
+                {
+                    yesno = true;
+                    Console.WriteLine("Введено не число");
+                    break;
+                }
+            }
+            if (yesno == false)
+            {
+                Console.WriteLine(razn.Aggregate((x, y) => x - y));
+            } 
+        }
+    }
+}
